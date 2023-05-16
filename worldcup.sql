6d7076d80851663adb0560ad2620b6d545e8a390
Juan-1978 <132173346+Juan-1978@users.noreply.github.com>
2023-05-16 11:41:08 -0400

    Update worldcup.sql

--- worldcup.sql
+++ worldcup.sql
@@ -88,7 +88,7 @@ ALTER SEQUENCE public.games_game_id_seq OWNED BY public.games.game_id;
 
 CREATE TABLE public.teams (
     team_id integer NOT NULL,
-    name text NOT NULL
+    name character varying(30) NOT NULL
 );
 
 
